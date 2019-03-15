2f0e50015381341a3b0b8693c2339963b134f70d
Mark Szymczyk <mark@meandmark.com>
2019-03-15 15:58:37 -0400

    Initial Commit

---
+++ ButtonClickCounter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ButtonClickCounter
+//
+//  Created by mark on 3/15/19.
+//  Copyright © 2019 Swift Dev Journal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
